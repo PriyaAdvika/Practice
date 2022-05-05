9dd1b462d92ee58e58f72f28e1ecf495b2616422
Priyanka Anand <priyankagupta441@gmail.com>
2022-05-05 21:29:50 +0530

    Initial commit

---
+++ Test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    int sum = 0;
+
+    printf("Enter First Number:");
+    scanf(&a);
+    printf("\nEnter Secon Number:");
+    scanf(&b);
+
+    sum = a+b;
+
+    printf("\nSum = %d",sum);
+
+
+    return 0;
+}
+
